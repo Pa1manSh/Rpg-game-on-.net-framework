548a3c9744005444c9a16941010124057d830001
Pa1manSh <121258024+Pa1manSh@users.noreply.github.com>
2022-12-23 10:30:45 +0300

    Add files via upload

---
+++ RPG/Const.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    public static class Const
+    {
+        // Damage Multiplier
+        public const int damageMulti = 10;
+        // chance multiplier to dodge
+        public const int dodgeMulti = 6;
+        //health points multiplier
+        public const int hpMulti = 100;
+        // damage spread multiplier
+        public const int damageVar = 10;
+        // number of skill points
+        public const int pointsNumber = 5;
+    }
+}
---
+++ RPG/Hero/Warrior.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Hero
+{
+    internal class Warrior: CharacterBase
+    {
+        public Warrior(string name = "the name must be chosen by the player") : base(name, " Warrior", "Rage - Pain only makes a warrior stronger. After the blow, the warrior becomes enraged and thrice hits the opponent with a shield. The damage of each hit is equal to the strength indicator", 4, 2, 4)
+        {
+
+        }
+
+        public override int UseUltimateAbility()
+        {
+            int ult = Strength * 3;
+            Console.WriteLine($"{Name} gets into a rage and hits 3 times with a shield, causing {ult} damage");
+            return ult;
+        }
+
+    }
+}
